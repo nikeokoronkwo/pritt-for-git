23340f9d6fcf39ac178f610e39c9f6244bcbeded
--add <nikechukwu@gmail.com>
2024-04-25 00:27:01 -0400

    Added private env package

---
+++ packages/env/example/env_example.dart
@@ -0,0 +1,6 @@
+import 'package:env/env.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/env/test/env_test.dart
@@ -0,0 +1,16 @@
+import 'package:env/env.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
