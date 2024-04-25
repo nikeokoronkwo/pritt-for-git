a8996f4a92d14204f356e6449b8253ae22d13fd7
--add <nikechukwu@gmail.com>
2024-04-25 12:13:57 -0400

    Documentation

--- packages/env/lib/src/env_base.dart
+++ packages/env/lib/src/env_base.dart
@@ -1,3 +1,13 @@
+
+/// The `env` annotation, used to bind an enviroment variable value at the root of a project to a value in the dart application.
+/// Whenever the annotation is initialised, the name of the environment variable, [name], is passed.
+/// 
+/// ```dart
+/// @env("MY_ENV_VARIABLE")
+/// String myVar = MY_ENV_VARIABLE;
+/// ```
+/// 
+/// The generated code for the variable `MY_ENV_VARIABLE` will be built as a part builder to the file containing the `@env` annotation
 // ignore: camel_case_types
 class env {
   final String name;
