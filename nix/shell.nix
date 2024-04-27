9d0ac986bcef9f8105d6457099f99ba1c038bdbd
--add <nikechukwu@gmail.com>
2024-04-27 17:56:51 -0400

    added code for shell.nix file

---
+++ nix/shell.nix
@@ -0,0 +1,13 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    ruby
+    dart
+    go
+    nodejs_21
+  ];
+}
