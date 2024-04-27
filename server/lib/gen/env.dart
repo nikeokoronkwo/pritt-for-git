cf4762be1176130c9c97337d28cbeafb78c3428d
--add <nikechukwu@gmail.com>
2024-04-27 13:53:16 -0400

    Updated server environments

--- server/lib/gen/env.dart
+++ server/lib/gen/env.dart
@@ -2,8 +2,11 @@ import 'package:env/env.dart';
 
 part 'env.g.dart';
 
-@env('DATA_ENTRY')
+@env('DATA_JSON')
 String dataPath = DATA_ENTRY;
 
 @env('CLIENT_DIR')
 String client = CLIENT_DIR;
+
+@env('SERVICES_DIR')
+String services = SERVICES_DIR;
